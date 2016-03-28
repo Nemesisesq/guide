9301d1197a919be308182912db3dff5f677dde43
Carl <nemesisesq@gmail.com>
2016-03-27 21:32:04 -0400

    styled the guide

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,9 @@
 angular.module('starter.controllers', [])
 
   .controller('GuideController', function ($scope, GuideFactory, _, $window, $rootScope) {
-    if(_.isEmpty($window.sessionStorage.token)){
+    if (_.isEmpty($window.sessionStorage.token)) {
       //debugger
-      $scope.$emit('show_login',[])
+      $scope.$emit('show_login', [])
 
     }
     $scope.guide = []
@@ -15,9 +15,13 @@ angular.module('starter.controllers', [])
     //  })
     $scope.hello = 'world'
 
-    $('ion-content').scroll(function(){
+    $('ion-content').scroll(function () {
       left = $(this).find('.scroll').position().left
-      $(this).find('.channel').css({'left' : -(left) + 10})
+      if (left < 0) {
+        $(this).find('.channel').css({'left': -(left)})
+      } else {
+        $(this).find('.channel').css({'left': 0})
+      }
 
     })
 
@@ -35,7 +39,7 @@ angular.module('starter.controllers', [])
     var h = angular.element('.title').height()
     console.log(h)
 
-    $('.search').css('margin-top',h +"px")
+    $('.search').css('margin-top', h + "px")
 
     $scope.test = _.range(100)
 
@@ -43,37 +47,37 @@ angular.module('starter.controllers', [])
 
   })
 
-//.controller('ChatsCtrl', function($scope, Chats) {
-//  // With the new view caching in Ionic, Controllers are only called
-//  // when they are recreated or on app start, instead of every page change.
-//  // To listen for when this page is active (for example, to refresh data),
-//  // listen for the $ionicView.enter event:
-//  //
-//  //$scope.$on('$ionicView.enter', function(e) {
-//  //});
-//
-//  $scope.chats = Chats.all();
-//  $scope.remove = function(chat) {
-//    Chats.remove(chat);
-//  };
-//})
-//
-//.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-//  $scope.chat = Chats.get($stateParams.chatId);
-//})
-//
-.controller('AccountCtrl', function($scope, $http, ENDPOINT) {
-  $scope.getGuide = function () {
-    debugger;
-    $http.get('http://titantv.com/')
-      .then(function(data){
-        $http.post(ENDPOINT.url + '/guide_reciever/', data.data)
-          .then(function (data) {
-            console.log(data)
+  //.controller('ChatsCtrl', function($scope, Chats) {
+  //  // With the new view caching in Ionic, Controllers are only called
+  //  // when they are recreated or on app start, instead of every page change.
+  //  // To listen for when this page is active (for example, to refresh data),
+  //  // listen for the $ionicView.enter event:
+  //  //
+  //  //$scope.$on('$ionicView.enter', function(e) {
+  //  //});
+  //
+  //  $scope.chats = Chats.all();
+  //  $scope.remove = function(chat) {
+  //    Chats.remove(chat);
+  //  };
+  //})
+  //
+  //.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  //  $scope.chat = Chats.get($stateParams.chatId);
+  //})
+  //
+  .controller('AccountCtrl', function ($scope, $http, ENDPOINT) {
+    $scope.getGuide = function () {
+      debugger;
+      $http.get('http://titantv.com/')
+        .then(function (data) {
+          $http.post(ENDPOINT.url + '/guide_reciever/', data.data)
+            .then(function (data) {
+              console.log(data)
 
-          })
-      })
-  }
+            })
+        })
+    }
 
-  $scope.loginForm = true;
-});
+    $scope.loginForm = true;
+  });
